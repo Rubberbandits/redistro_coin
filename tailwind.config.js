bde802fd9992cf158c91c1b3e03b1b3e47a6adfe
rusty <dalegribble@riseup.net>
2021-12-10 10:09:24 -0800

    add hero to home page

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,9 +5,15 @@ module.exports = {
 	],
 	theme: {
 		extend: {
+			zIndex: {
+				'1000': '1000',
+			},
 			spacing: {
 				"128": "32rem",
 				'144': '36rem',
+			},
+			backgroundImage: {
+				'hero': "url('/expand.jpg')",
 			}
 		},
 	},

63331fa4bc9bd6ea92cf2c67a73d1baabe961b81
rusty <dalegribble@riseup.net>
2021-12-12 10:14:04 -0800

    update icons

--- pages/index.js
+++ pages/index.js
@@ -38,7 +38,7 @@ export default function Home() {
 							<button className="btn btn-primary btn-wide mb-2 sm:w-min sm:mb-0 sm:mr-5">Discord</button>
 						</a>
 						<a href="https://twitter.com/SimulaCoin">
-						<button className="btn btn-primary btn-wide sm:w-min sm:mb-0 sm:mr-5">Twitter</button>
+							<button className="btn btn-primary btn-wide sm:w-min sm:mb-0 sm:mr-5">Twitter</button>
 						</a>
 					</nav>
 				</div>
@@ -78,8 +78,7 @@ export default function Home() {
 							<div className="p-4 md:w-1/4 sm:w-1/2 w-full">
 								<div className="border-2 border-gray-200 px-4 py-6 rounded-lg">
 									<svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="text-primary w-12 h-12 mb-3 inline-block" viewBox="0 0 24 24">
-										<path d="M8 17l4 4 4-4m-4-5v9"></path>
-										<path d="M20.88 18.09A5 5 0 0018 9h-1.26A8 8 0 103 16.29"></path>
+										<path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 8c-1.657 0-3 .895-3 2s1.343 2 3 2 3 .895 3 2-1.343 2-3 2m0-8c1.11 0 2.08.402 2.599 1M12 8V7m0 1v8m0 0v1m0-1c-1.11 0-2.08-.402-2.599-1M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
 									</svg>
 
 									<h2 className="title-font font-medium text-lg xl:text-3xl">100,000,000,000</h2>
@@ -103,8 +102,8 @@ export default function Home() {
 							<div className="p-4 md:w-1/4 sm:w-1/2 w-full">
 								<div className="border-2 border-gray-200 px-4 py-6 rounded-lg">
 									<svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="text-primary w-12 h-12 mb-3 inline-block" viewBox="0 0 24 24">
-										<path d="M3 18v-6a9 9 0 0118 0v6"></path>
-										<path d="M21 19a2 2 0 01-2 2h-1a2 2 0 01-2-2v-3a2 2 0 012-2h3zM3 19a2 2 0 002 2h1a2 2 0 002-2v-3a2 2 0 00-2-2H3z"></path>
+										<path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M11 3.055A9.001 9.001 0 1020.945 13H11V3.055z" />
+										<path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M20.488 9H15V3.512A9.025 9.025 0 0120.488 9z" />
 									</svg>
 
 									<h2 className="title-font font-medium text-lg xl:text-3xl">2%</h2>
